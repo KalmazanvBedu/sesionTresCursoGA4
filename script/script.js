9f6d644e044b53276aa2ce48c625caf8c206a4ce
Keny 1 <keny.almazan@mrm.com>
2024-03-25 11:46:45 -0600

    Se agrega GA4

--- script/script.js
+++ script/script.js
@@ -1,1 +1,71 @@
+// Ejercicios if y switch
+
+// const enlace1 = document.getElementById("enlace1");
+
+// var decision = 4;
+
+// Bloque 1
+// if(decision == 1){
+//     enlace1.addEventListener('click',
+//     function(){
+
+//         alert("Click en enlace 1 por camino 1");
+
+//     }
+// )
+// }
+
+//********** Bloque switch *********
+
+// switch(decision) {
+//     case 1:
+//         enlace1.addEventListener('click',
+//             function(){
+        
+//                 alert("Click en enlace 1 por camino 1");
+        
+//             }
+//         )
+//       break;
+//     case 2:
+//         enlace1.addEventListener('click',
+//             function(){
+        
+//                 alert("Click en enlace 1 por camino 2");
+        
+//             }
+//         )
+//       break;
+//     default:
+//         enlace1.addEventListener('click',
+//             function(){
+        
+//                 alert("Click en otro camino");
+        
+//             }
+//         )
+//   }
+
+// const enlace1 = document.getElementById("enlace1");
+
+// var listaNombres = ["Karla","Tián","Mario","Keny","Erick","Vale","Juls"];
+
+// for(let i = 0; i<=6; i++){
+
+//     enlace1.addEventListener('click',
+//             function(){
+        
+//                 alert("Hola " + listaNombres[i]);
+        
+//             }
+//         )
+
+// }
+
+
+var i=1;
+while (i <= 10) {
+    console.log("Vuelta" + i);
+    i++;
+  }
 
